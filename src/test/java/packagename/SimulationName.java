69d58f19bee0063e059c1af192c3ef998fa6f7db
katwaGatling <katwa@gatling.io>
2024-05-23 13:13:16 +0200

    update bundle structure

---
+++ src/test/java/packagename/SimulationName.java
@@ -0,0 +1,25 @@
+package packagename;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+public class SimulationName extends Simulation {
+
+    public static final int vu = Integer.getInteger("vu", 1);
+
+    HttpProtocolBuilder httpProtocol =
+        http.baseUrl("https://computer-database.gatling.io")
+        .acceptHeader("application/json")
+        .contentTypeHeader("application/json");
+
+    ScenarioBuilder users = scenario("My Scenario").exec(http("Request 1").get("/computers"));
+
+    {
+        setUp(
+            users.injectOpen(atOnceUsers(vu))
+        ).protocols(httpProtocol);
+    }
+}
